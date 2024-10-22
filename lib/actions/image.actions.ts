4201ac5608f73c72362e06fdeb5e5d1df188fce0
Dennis Kimtai <kimtai.developer@gmail.com>
2024-10-22 13:19:05 +0300

    fix ts

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -9,6 +9,7 @@ import { redirect } from "next/navigation";
 
 import { v2 as cloudinary } from 'cloudinary'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const populateUser = (query: any) => query.populate({
     path: 'author',
     model: User,
@@ -118,6 +119,7 @@ export async function getAllImages({ limit = 9, page = 1, searchQuery = '' }: {
         .expression(expression)
         .execute();
 
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         const resourceIds = resources.map((resource: any) => resource.public_id);
 
         let query = {};
